855d13d3bc984c5ce6617852c39e497119a4c7fd
arya2004 <arya20j4@gmail.com>
2023-07-27 14:58:30 +0530

    added home pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './home/team/team.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent}
+  {path:'', component: HomeComponent},
+  {path:'team', component: TeamComponent},
+  {path:'**', redirectTo:'',pathMatch:'full'}
 ];
 
 @NgModule({
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { TeamComponent } from './team/team.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    TeamComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
-    HomeComponent
+    HomeComponent,
+    TeamComponent
   ]
 })
 export class HomeModule { }
