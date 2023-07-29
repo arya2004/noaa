3d755ebaa86f0f5bd62e57ae0d2206565e09ebf0
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2023-07-29 19:45:42 +0530

    Committed ClassroomAPI and SubmissionAPI by Omkar Lolage

---
+++ Omkar/ClassroomAPI/src/main/java/com/example/demo/ClassroomApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomApiApplication.class, args);
+	}
+
+}
