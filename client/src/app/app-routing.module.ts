40738fa991a306fb2d0df2960209c9c9d10f3158
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2023-11-25 23:46:32 +0530

    no message

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TeamComponent } from './home/team/team.component';
+import { LoginPageComponent } from './login/login-page.component';
+import { SignUpPageComponent } from    './home/sign-up-page/sign-up-page.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'team', component: TeamComponent},
   {path:'**', redirectTo:'',pathMatch:'full'}
+      {path:"", redirectTo:"login", pathMatch:"full"},
+    {path:"login", component:LoginPageComponent},
+    {path:"signUp", component:SignUpPageComponent},
+    {path:"home", component:HomeComponent},
 ];
 
 @NgModule({
